983edf5b5f95888d9b7b5b3db8b6dea661940f5b
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-07-09 13:12:49 +0200

    handle in memory price change

--- src/index.js
+++ src/index.js
@@ -7,23 +7,28 @@ const { buildPriceUpdateMessage } = require("./slackMessages.js");
 const { productsRepository } = require("./productsRepository.js");
 
 //"0/4 * 9-23 * * *"
-const job = schedule.scheduleJob("0/4 * * * * *", async function () {
+const job = schedule.scheduleJob("*/30 * * * *", async function () {
   const browser = await puppeteer.launch();
 
   for (var product of productsRepository.getAllProducts()) {
+    console.log(`Fetching ${product.name} price`);
     for (var page of product.pages) {
+      const { lastPrice, url, priceElemId } = page;
       const browserPage = await browser.newPage();
-      await browserPage.goto(page.url);
+      await browserPage.goto(url);
       const price = await browserPage
-        .$(`#${page.priceElemId}`)
+        .$(`#${priceElemId}`)
         .then((element) => element.getProperty("innerText"))
         .then((handle) => handle.jsonValue());
 
-      //TODO check if price has actually changed
-      await axios.post(
-        process.env.SLACK_WEBHOOK_URL,
-        buildPriceUpdateMessage(product.name, price, page.url, product.photoUrl)
-      );
+      if (!lastPrice || lastPrice !== price) {
+        await axios.post(
+          process.env.SLACK_WEBHOOK_URL,
+          buildPriceUpdateMessage(product.name, price, url, product.photoUrl)
+        );
+      }
+
+      page.lastPrice = price;
     }
   }
 
